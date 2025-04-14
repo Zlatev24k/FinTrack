c0d3c529a7d099e7fe35c174dbe7f14c2d5bc792
EmreSuleiman <emreshaib@gmail.com>
2025-04-14 11:04:43 +0300

    Delete Income

--- FormViews/Income.Designer.cs
+++ FormViews/Income.Designer.cs
@@ -30,6 +30,9 @@
         {
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            label4 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
             textBox1 = new TextBox();
             button2 = new Button();
             button1 = new Button();
@@ -39,20 +42,21 @@
             label2 = new Label();
             listBox2 = new ListBox();
             tabPage3 = new TabPage();
-            tabPage4 = new TabPage();
-            textBox2 = new TextBox();
-            label3 = new Label();
-            label4 = new Label();
-            listBox3 = new ListBox();
-            label5 = new Label();
-            textBox3 = new TextBox();
-            label6 = new Label();
-            button3 = new Button();
             button4 = new Button();
+            button3 = new Button();
+            label6 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            listBox3 = new ListBox();
+            tabPage4 = new TabPage();
+            listBox4 = new ListBox();
+            label7 = new Label();
+            button5 = new Button();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
             tabPage2.SuspendLayout();
             tabPage3.SuspendLayout();
+            tabPage4.SuspendLayout();
             SuspendLayout();
             // 
             // tabControl1
@@ -85,6 +89,34 @@
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Add Income";
             tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10F);
+            label4.Location = new Point(277, 335);
+            label4.Name = "label4";
+            label4.Size = new Size(76, 23);
+            label4.TabIndex = 7;
+            label4.Text = "Amount:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10F);
+            label3.Location = new Point(277, 56);
+            label3.Name = "label3";
+            label3.Size = new Size(249, 23);
+            label3.TabIndex = 6;
+            label3.Text = "Write name fot type of Income:";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F);
+            textBox2.Location = new Point(277, 358);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(257, 34);
+            textBox2.TabIndex = 5;
             // 
             // textBox1
             // 
@@ -178,69 +210,24 @@
             tabPage3.Text = "Update Income";
             tabPage3.UseVisualStyleBackColor = true;
             // 
-            // tabPage4
+            // button4
             // 
-            tabPage4.Location = new Point(4, 29);
-            tabPage4.Name = "tabPage4";
-            tabPage4.Padding = new Padding(3);
-            tabPage4.Size = new Size(792, 417);
-            tabPage4.TabIndex = 3;
-            tabPage4.Text = "Delete Income";
-            tabPage4.UseVisualStyleBackColor = true;
+            button4.Font = new Font("Segoe UI", 12F);
+            button4.Location = new Point(383, 187);
+            button4.Name = "button4";
+            button4.Size = new Size(197, 67);
+            button4.TabIndex = 5;
+            button4.Text = "Update Amount";
+            button4.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // button3
             // 
-            textBox2.Font = new Font("Segoe UI", 12F);
-            textBox2.Location = new Point(277, 358);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(257, 34);
-            textBox2.TabIndex = 5;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 10F);
-            label3.Location = new Point(277, 56);
-            label3.Name = "label3";
-            label3.Size = new Size(249, 23);
-            label3.TabIndex = 6;
-            label3.Text = "Write name fot type of Income:";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 10F);
-            label4.Location = new Point(277, 335);
-            label4.Name = "label4";
-            label4.Size = new Size(76, 23);
-            label4.TabIndex = 7;
-            label4.Text = "Amount:";
-            // 
-            // listBox3
-            // 
-            listBox3.FormattingEnabled = true;
-            listBox3.Location = new Point(23, 64);
-            listBox3.Name = "listBox3";
-            listBox3.Size = new Size(254, 324);
-            listBox3.TabIndex = 0;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 15F);
-            label5.Location = new Point(23, 26);
-            label5.Name = "label5";
-            label5.Size = new Size(171, 35);
-            label5.TabIndex = 1;
-            label5.Text = "Your Incomes:";
-            // 
-            // textBox3
-            // 
-            textBox3.Font = new Font("Segoe UI", 15F);
-            textBox3.Location = new Point(383, 64);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(197, 41);
-            textBox3.TabIndex = 2;
+            button3.Location = new Point(529, 153);
+            button3.Name = "button3";
+            button3.Size = new Size(8, 8);
+            button3.TabIndex = 4;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
             // 
             // label6
             // 
@@ -252,24 +239,72 @@
             label6.TabIndex = 3;
             label6.Text = "New Amount:";
             // 
-            // button3
+            // textBox3
             // 
-            button3.Location = new Point(529, 153);
-            button3.Name = "button3";
-            button3.Size = new Size(8, 8);
-            button3.TabIndex = 4;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
+            textBox3.Font = new Font("Segoe UI", 15F);
+            textBox3.Location = new Point(383, 64);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(197, 41);
+            textBox3.TabIndex = 2;
             // 
-            // button4
+            // label5
             // 
-            button4.Font = new Font("Segoe UI", 12F);
-            button4.Location = new Point(383, 187);
-            button4.Name = "button4";
-            button4.Size = new Size(197, 67);
-            button4.TabIndex = 5;
-            button4.Text = "Update Amount";
-            button4.UseVisualStyleBackColor = true;
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15F);
+            label5.Location = new Point(23, 26);
+            label5.Name = "label5";
+            label5.Size = new Size(171, 35);
+            label5.TabIndex = 1;
+            label5.Text = "Your Incomes:";
+            // 
+            // listBox3
+            // 
+            listBox3.FormattingEnabled = true;
+            listBox3.Location = new Point(23, 64);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(254, 324);
+            listBox3.TabIndex = 0;
+            // 
+            // tabPage4
+            // 
+            tabPage4.Controls.Add(button5);
+            tabPage4.Controls.Add(label7);
+            tabPage4.Controls.Add(listBox4);
+            tabPage4.Location = new Point(4, 29);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Padding = new Padding(3);
+            tabPage4.Size = new Size(792, 417);
+            tabPage4.TabIndex = 3;
+            tabPage4.Text = "Delete Income";
+            tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // listBox4
+            // 
+            listBox4.FormattingEnabled = true;
+            listBox4.Location = new Point(23, 57);
+            listBox4.Name = "listBox4";
+            listBox4.Size = new Size(254, 324);
+            listBox4.TabIndex = 1;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 15F);
+            label7.Location = new Point(23, 19);
+            label7.Name = "label7";
+            label7.Size = new Size(171, 35);
+            label7.TabIndex = 2;
+            label7.Text = "Your Incomes:";
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 12F);
+            button5.Location = new Point(369, 168);
+            button5.Name = "button5";
+            button5.Size = new Size(197, 67);
+            button5.TabIndex = 6;
+            button5.Text = "Delete Income";
+            button5.UseVisualStyleBackColor = true;
             // 
             // Income
             // 
@@ -286,6 +321,8 @@
             tabPage2.PerformLayout();
             tabPage3.ResumeLayout(false);
             tabPage3.PerformLayout();
+            tabPage4.ResumeLayout(false);
+            tabPage4.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -312,5 +349,8 @@
         private Button button3;
         private Label label6;
         private TextBox textBox3;
+        private ListBox listBox4;
+        private Button button5;
+        private Label label7;
     }
 }
